737edf28edc87cba1aee5a1b3935dda67d2ae764
Bauke Brenninkmeijer <bauke.brenninkmeijer@gmail.com>
2024-10-11 20:38:15 +0200

    update data location

--- tabular_titans/data_generation.py
+++ tabular_titans/data_generation.py
@@ -1,14 +1,15 @@
+from pathlib import Path
 from typing import Any
 
 import numpy as np
 import pandas as pd
 import polars as pl
 from tqdm import trange
-from pathlib import Path
 
-data_location = Path(__file__).parent.parent / 'data'
+data_location = Path(__file__).parent.parent / "data"
 if not data_location.exists():
     data_location.makedirs(exist_ok=True)
+
 
 # Function to generate fake e-commerce data
 def generate_fake_data(num_rows) -> dict[str, Any]:
@@ -19,25 +20,28 @@ def generate_fake_data(num_rows) -> dict[str, Any]:
         "product_id": np.random.randint(1, 10000, num_rows),
         "quantity": np.random.randint(1, 10, num_rows),
         "price": np.random.uniform(10, 1000, num_rows).round(2),
-        "order_date": random_dates(pd.to_datetime('2022-1-1'), pd.to_datetime('2025-1-1'), num_rows),
+        "order_date": random_dates(
+            pd.to_datetime("2022-1-1"), pd.to_datetime("2025-1-1"), num_rows
+        ),
     }
     return data
+
 
 def save_fake_data_n_times(n, num_rows):
     for i in trange(n):
         data = generate_fake_data(num_rows)
         df = pl.DataFrame(data)
-        df.write_parquet(f'data/chunk_{i}.parquet')
-        print(f'Saved chunk {i}')
+        df.write_parquet(data_location / f"chunk_{i}.parquet")
+        print(f"Saved chunk {i}")
+
 
 def random_dates(start, end, n=10):
-    start_u = start.value//10**9
-    end_u = end.value//10**9
+    start_u = start.value // 10**9
+    end_u = end.value // 10**9
     return pl.from_epoch(
-        pl.Series(
-            np.random.randint(start_u, end_u, n)
-        ),
-        time_unit='ms')
+        pl.Series(np.random.randint(start_u, end_u, n)), time_unit="ms"
+    )
+
 
 # Generate data
 num_rows = 10_000_000
